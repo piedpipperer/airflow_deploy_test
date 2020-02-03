238c5938db2959c57db483816e97b66f4629d04d
Jordi <jordirojo@gmail.com>
2020-02-03 10:14:48 +0100

    'testing_dag_changes'

--- app/dags/tutorial_docum_dag.py
+++ app/dags/tutorial_docum_dag.py
@@ -14,8 +14,8 @@ default_args = {
     'email_on_failure': ['jobbing.314@gmail.com'],
     'email_on_retry': ['jobbing.314@gmail.com'],
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-    # 'queue': 'bash_queue',
+    'retry_delay': timedelta(minutes=5)
+    #, 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
